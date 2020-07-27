0aa634df4d36ea39de3618286d0b0421281eba97
evelyn-ma03 <68654838+evelyn-ma03@users.noreply.github.com>
2020-07-27 13:55:34 -0400

    Completed Iteration 0

---
+++ ToDoList/ToDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Evelyn Ma on 7/27/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import Foundation
+
+class ToDoClass {
+    var description = ""
+    var important = false
+}
